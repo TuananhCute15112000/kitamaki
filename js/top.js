94e52113964f74c3c932b4657426892e6cfceb6a
TuananhCute15112000 <lt-anh@b-create.co.jp>
2023-03-13 17:21:42 +0700

    csc

--- js/top.js
+++ js/top.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
   "use strict";
+  Splitting();
+  new universalParallax().init({
+    speed: 8.0,
+  });
+  // slider 1
   const swiper1 = new Swiper(".swiper1", {
     spaceBetween: 0,
     centeredSlides: true,
     speed: 6000,
     autoplay: {
       delay: 1,
-      reverseDirection: true
     },
     loop: true,
     loopedSlides: 4,
@@ -14,6 +18,7 @@ $(document).ready(function () {
     allowTouchMove: false,
     disableOnInteraction: true
   });
+  // slider 2
   const swiper2 = new Swiper(".swiper2", {
     autoplay: {
       delay: 0, // 途切れなくループ
@@ -26,56 +31,82 @@ $(document).ready(function () {
     speed: 6000, // ループの時間
     allowTouchMove: false, // スワイプ無効
   });
+
+  // slider mainvisual
+  var swiper = new Swiper(".mySwiper", {
+    centeredSlides: true,
+    effect: 'fade',
+    speed: 1000,
+    loop: true,
+    // autoplay: {
+    //     delay: 3000,
+    //     disableOnInteraction: false,
+    // },
 });
 
-jQuery(document).ready(function ($) {
-  let autoPlayDelay = 1500;
-  let options = {
-    init: true,
-    slidesPerView: 1,
-    loop: false,
-    effect: "fade",
+  // slider report
+  // menu
+  $(window).bind('scroll', function () {
+    const height = $("#mainvisual").height();
+    if ($(window).scrollTop() > height) {
+      $('.menu_icon').addClass('scroll')
+    } else {
+      $('.menu_icon').removeClass('scroll')
+    }
+  });
+  // slider party report
+  var swiper = new Swiper(".swiper-container", {
     autoplay: {
-      delay: autoPlayDelay
+      delay: 4000,
+      disableOnInteraction: false
+    },
+    speed: 500,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction"
     },
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
     },
-  };
-  let mySwiper = new Swiper('.swiper-container', options);
-  let slidersCount = mySwiper.params.loop ? mySwiper.slides.length - 2 : mySwiper.slides.length;
-  let widthParts = 100 / slidersCount;
-
-  $('.swiper-counter .total').html(slidersCount);
-  for (let i = 0; i < slidersCount; i++) {
-    $('.swiper-progress-bar .progress-sections').append('<span></span>');
-  }
-  function initProgressBar() {
-    let calcProgress = (slidersCount - 1) * (autoPlayDelay + mySwiper.params.speed);
-    calcProgress += autoPlayDelay;
-    $('.swiper-progress-bar .progress').animate({
-      width: '100%'
-    }, calcProgress, 'linear');
-  }
-  initProgressBar();
-  mySwiper.on('slideChange', function () {
-    let progress = $('.swiper-progress-bar .progress');
-    $('.swiper-counter .current').html(this.activeIndex + 1);
-
-    if ( ( this.progress == -0 || (this.progress == 1 && this.params.loop) ) && !progress.parent().is('.stopped')) {
-      progress.css('width', '0');
-      if (this.activeIndex == 0) {
-        initProgressBar();
+    on: {
+      init: function () {
+        $(".swiper-progress-bar").removeClass("animate");
+        $(".swiper-progress-bar").removeClass("active");
+        $(".swiper-progress-bar").eq(0).addClass("animate");
+        $(".swiper-progress-bar").eq(0).addClass("active");
+      },
+      slideChangeTransitionStart: function () {
+        $(".swiper-progress-bar").removeClass("animate");
+        $(".swiper-progress-bar").removeClass("active");
+        $(".swiper-progress-bar").eq(0).addClass("active");
+      },
+      slideChangeTransitionEnd: function () {
+        $(".swiper-progress-bar").eq(0).addClass("animate");
       }
     }
-    if (progress.parent().is('.stopped')) {
-      progress.animate({
-        'width': Math.round(widthParts * (this.activeIndex + 1)) + '%'
-      }, this.params.speed, 'linear');
-    }
   });
-  mySwiper.on('touchMove', function () {
-    $('.swiper-progress-bar .progress').stop().parent().addClass('stopped');
+  $(".swiper-container").hover(function () {
+    swiper.autoplay.stop();
+    $(".swiper-progress-bar").removeClass("animate");
+  }, function () {
+    swiper.autoplay.start();
+    $(".swiper-progress-bar").addClass("animate");
   });
+
+  // animate text
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.from('#title1', { duration: 1, y: '200', opacity: 0, ease: 'slow', scrollTrigger: { trigger:"#title1",start:"top 95%",end:"bottom 40%", toggleActions:"restart complete reverse reset" }});
+  gsap.from('#title2', { duration: 1, y: '200', opacity: 0, ease: 'slow', scrollTrigger: { trigger:"#title2",start:"top 95%",end:"bottom 40%", toggleActions:"restart complete reverse reset" }})
+  gsap.from('#title3', { duration: 1, y: '200', opacity: 0, ease: 'slow', scrollTrigger: { trigger:"#title3",start:"top 95%",end:"bottom 40%", toggleActions:"restart complete reverse reset" }})
+  gsap.from('#title4', { duration: 1, y: '200', opacity: 0, ease: 'slow', scrollTrigger: { trigger:"#title4",start:"top 95%",end:"bottom 40%", toggleActions:"restart complete reverse reset" }})
+  gsap.from('#title5', { duration: 1, y: '200', opacity: 0, ease: 'slow', scrollTrigger: { trigger:"#title5",start:"top 95%",end:"bottom 40%", toggleActions:"restart complete reverse reset" }})
+  gsap.from('#title6', { duration: 1, y: '200', opacity: 0, ease: 'slow', scrollTrigger: { trigger:"#title6",start:"top 95%",end:"bottom 40%", toggleActions:"restart complete reverse reset" }})
+  gsap.from('#text1', { duration: 1, y: '200', opacity: 0, ease: 'slow', scrollTrigger: { trigger:"#text1",start:"top 95%",end:"bottom 40%", toggleActions:"restart complete reverse reset" }})
+  gsap.from('#text2', { duration: 1, y: '200', opacity: 0, ease: 'slow', scrollTrigger: { trigger:"#text2",start:"top 95%",end:"bottom 40%", toggleActions:"restart complete reverse reset" }})
+  gsap.from('#text3', { duration: 1, y: '200', opacity: 0, ease: 'slow', scrollTrigger: { trigger:"#text3",start:"top 95%",end:"bottom 40%", toggleActions:"restart complete reverse reset" }})
+  gsap.from('#image1', { duration: 1, y: '200', opacity: 0, ease: 'slow', scrollTrigger: { trigger:"#image1",start:"top 95%",end:"bottom 40%", toggleActions:"restart complete reverse reset" }})
 });
+
+
